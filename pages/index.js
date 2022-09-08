48441ca28a64aa684e6b94ab2151e232c458d4e2
web3Codess <vercelploy@gmail.com>
2022-09-08 16:46:57 +0300

    ..

--- pages/index.js
+++ pages/index.js
@@ -18,21 +18,21 @@ const index = () => {
 
 
                <div className='flex justify-center mt-8 '>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300  text-white">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md    active:scale-90 hover:translate-y-2 transition-all border-t-2   text-white">
                 <Link href={`https://t.me/metavass`}>NFT MINTING SITE 
                </Link>
                 </button>
                 </div>
 
                <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300  text-center text-white">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2  text-center text-white">
                 <Link href={`https://t.me/metavass`}>NFT MINTING SITE 
                 GENERATE 10K NFT COLLECTION AND SELL</Link>
                 </button>
                 </div>
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md   shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-2  text-white ">
                 <Link href={`https://t.me/metavass`}>NFT MARKET PLACE LIKE OPENSEA
                 </Link>
                 </button>
@@ -42,14 +42,14 @@ const index = () => {
                
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-2  text-white ">
                 <Link href={`https://t.me/metavass`}>NFT WEBSITE
                 </Link>
                 </button>
                 </div>
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300  text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2   text-white ">
                 <Link href={`https://t.me/metavass`}>NFT SMART CONTRACT
                 </Link>
                 </button>
@@ -57,7 +57,7 @@ const index = () => {
 
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2 text-white ">
                 <Link href={`https://t.me/metavass`}>CRYPTO COIN DEVELOPMENT
                 </Link>
                 </button>
@@ -65,7 +65,7 @@ const index = () => {
 
              
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2 text-white ">
                 <Link href={`https://t.me/metavass`}>HOW TO FILTER SPAM NFTS
                 </Link>
                 </button>
@@ -75,14 +75,14 @@ const index = () => {
 
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300  text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2  text-white ">
                 <Link href={`https://t.me/metavass`}>HOW TO CANCEL A TRANSACTION ON ETHEREUM 
                 </Link>
                 </button>
                 </div>
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2 text-white ">
                 <Link href={`https://t.me/metavass`}>HOW TO CHECK OWNER OF AN NFT
                 </Link>
                 </button>
@@ -90,7 +90,7 @@ const index = () => {
 
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2  text-white ">
                 <Link href={`https://t.me/metavass`}> HOW TO CREATE WHALE ALERT BOTS
                 </Link>
                 </button>
@@ -99,7 +99,7 @@ const index = () => {
 
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2 text-white ">
                 <Link href={`https://t.me/metavass`} > CRYPTO EXCHANGE WEBSITE
                 </Link>
                 </button>
@@ -107,7 +107,7 @@ const index = () => {
 
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300  text-white ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl   active:scale-90 hover:translate-y-2 transition-all border-t-2 text-white ">
                 <Link href={`https://t.me/metavass`}  > WEB3 CONSULTATION
                 </Link>
                 </button>
